bef81e08101c778ab6954fd5e3bf1f6700bcda44
Josh <og.ginger@gmail.com>
2018-12-27 09:02:04 +0000

    CDN: Content Delivery Network.

---
+++ html/main.js
@@ -0,0 +1,10 @@
+//main.js: Main entry point for the linux, express, and node stack.
+
+var requirejs = require("requirejs");
+var config = require("./require_config");
+
+requirejs.config(config);
+
+requirejs([], function() {
+	console.log("main.js initialized successfully!");
+});
